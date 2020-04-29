c3fa01859ace40222e913db868c09e8b102e4c54
FichoZee <filip.zafran@gmail.com>
2020-04-29 23:03:44 +0200

    JQUERY GET

--- Asynchronous JavaScript/async.js
+++ Asynchronous JavaScript/async.js
@@ -22,11 +22,21 @@ window.onload = function () {
 
   // will be BEFORE our request as it is async (!)
   console.log("test");
-};
 
-// Value	State	            Description
-// 0	    UNSENT	            Client has been created. open() not called yet.
-// 1	    OPENED	            open() has been called.
-// 2	    HEADERS_RECEIVED	send() has been called, and headers and status are available.
-// 3	    LOADING	            Downloading; responseText holds partial data.
-// 4	    DONE	            The operation is complete.
+  // Value	State	            Description
+  // 0	    UNSENT	            Client has been created. open() not called yet.
+  // 1	    OPENED	            open() has been called.
+  // 2	    HEADERS_RECEIVED	send() has been called, and headers and status are available.
+  // 3	    LOADING	            Downloading; responseText holds partial data.
+  // 4	    DONE	            The operation is complete.
+
+  // ------------------------------------------------------
+
+  // JQUERY METHOD
+  // second function 'function (data)' will be fired async after first function "tweets.json"
+  $.get("tweets.json", function (data) {
+    console.log(data);
+  });
+
+  console.log("test");
+};
