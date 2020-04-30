4727173a17b1f559e793382dbe9d8e3f6cecc123
FichoZee <filip.zafran@gmail.com>
2020-04-30 13:31:41 +0200

    jquery Promises done

--- Asynchronous JavaScript/Asynchronous JavaScript #4 - Promises/async.js
+++ Asynchronous JavaScript/Asynchronous JavaScript #4 - Promises/async.js
@@ -1,14 +1,13 @@
 // https://www.youtube.com/watch?v=yswb4SkDoj0&t=24s
-// 3:25
 
 window.onload = function () {
   function get(url) {
-    return new Promise(function (reseolve, reject) {
-      var xhttp = new this.XMLHttpRequest();
+    return new Promise(function (resolve, reject) {
+      var xhttp = new XMLHttpRequest();
       xhttp.open("GET", url, true);
       xhttp.onload = function () {
         if (xhttp.status == 200) {
-          resolve(JSON.parse(http.response));
+          resolve(JSON.parse(xhttp.response));
         } else {
           reject(xhttp.statusText);
         }
@@ -21,7 +20,30 @@ window.onload = function () {
   }
 
   var promise = get("tweets.json");
-  promise.then(function (tweets) {
-    console.log(tweets);
-  });
+  promise
+    .then(function (tweets) {
+      console.log(tweets);
+      // WE CAN RETURN ANOTHER GET REQUEST(PROMISE) IN THIS CALLBACK FUNCTION
+      return get("friends.json");
+    })
+    .then(function (friends) {
+      console.log(friends);
+    })
+    // WE ADDED FRIENDS --------------------------------------
+    .catch(function (error) {
+      console.log(error);
+    });
 };
+
+// ovo gore mi je sve nejasno
+
+// JQUERY METHOD
+
+$.get("vids.json")
+  .then(function (vids) {
+    console.log(vids);
+    return $.get("friends.json");
+  })
+  .then(function (friends) {
+    console.log(friends);
+  });
